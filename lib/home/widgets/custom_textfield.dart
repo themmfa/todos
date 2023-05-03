82776f3c9a4874ba0d5c966baac8d57f78d4acf0
fe <fatiherdgn25@gmail.com>
2023-05-03 11:17:19 +0300

    lib/home/widgets: add custom_textfield

---
+++ lib/home/widgets/custom_textfield.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String hintText;
+
+  final TextEditingController controller;
+
+  const CustomTextField({
+    Key? key,
+    required this.hintText,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.blue),
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 24,
+        ),
+      ),
+    );
+  }
+}
