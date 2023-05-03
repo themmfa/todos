5cf7fc4b30727465c8317838ebed41444cb02026
fe <fatiherdgn25@gmail.com>
2023-05-03 12:49:48 +0300

    move home.dart to screen folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todos/home/bloc/home_bloc.dart';
-import 'package:todos/home/home.dart';
+import 'package:todos/home/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
